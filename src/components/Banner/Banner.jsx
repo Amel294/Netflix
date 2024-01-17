74180625926d2a42cbf55f55d0e78107fb74e230
Amel294 <amel294@gmail.com>
2024-01-17 09:45:53 +0530

    first commit

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,18 @@
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className="banner">
+            <div className="content">
+                <h1 className="title">Movie Name</h1>
+                <div className="banner-button">
+                    <button className="button">Play</button>
+                    <button className="button">My List`</button>
+                </div>
+                <h1 className="description">This is just a dummy paragraph This is just a dummy paragraph This is just a dummy paragraph</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
